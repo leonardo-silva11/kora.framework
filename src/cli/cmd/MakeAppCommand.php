59a5acb9b743c2aee81a8890306eeb09172a8744
Leonardo Souza <lss.leonardo.dev@gmail.com>
2023-10-12 13:54:10 -0300

    Criando comando para gerar app

---
+++ src/cli/cmd/MakeAppCommand.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class MakeAppCommand extends Command
+{
+    protected static $defaultName = 'make:app';
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Cria um novo app com arquivos de view')
+            ->setHelp('Este comando cria um novo app web com estrtutura de view.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        // Implemente a lógica para criar a estrutura do aplicativo aqui
+        // Por exemplo, criar controladores e arquivos necessários
+        $output->writeln('Criando um novo aplicativo...');
+
+        return Command::SUCCESS;
+    }
+}
