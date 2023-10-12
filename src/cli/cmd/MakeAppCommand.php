d03b63b1d252f3650f27690ebea250b32190582e
Leonardo Souza <lss.leonardo.dev@gmail.com>
2023-10-12 15:13:33 -0300

    Criando o comando para gerar um app

--- src/cli/cmd/MakeAppCommand.php
+++ src/cli/cmd/MakeAppCommand.php
@@ -4,6 +4,7 @@ namespace kora\cli\cmd;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
 
 class MakeAppCommand extends Command
 {
@@ -13,15 +14,21 @@ class MakeAppCommand extends Command
     {
         $this
             ->setDescription('Cria um novo app com arquivos de view')
-            ->setHelp('Este comando cria um novo app web com estrtutura de view.');
+            ->setHelp('Este comando cria um novo app web com estrutura de view.')
+            ->addArgument('appName', InputArgument::REQUIRED, 'Nome do aplicativo (obrigatório)');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        // Implemente a lógica para criar a estrutura do aplicativo aqui
+      
+
+        $appName = $input->getArgument('appName');
+
+        var_dump($appName);exit;
+          // Implemente a lógica para criar a estrutura do aplicativo aqui
         // Por exemplo, criar controladores e arquivos necessários
         $output->writeln('Criando um novo aplicativo...');
-
+        exit;
         return Command::SUCCESS;
     }
 }
